a9cfdb15712247aea87ecec6e1b9beb11d99ea34
Rangga Senatama <ranggasenatama@gmail.com>
2018-05-19 15:30:46 +0700

    add python ftp

---
+++ ftpclient.py
@@ -0,0 +1,22 @@
+from ftplib import FTP
+
+ftp = FTP('')
+ftp.connect('localhost',1026)
+ftp.login()
+ftp.cwd('download') #replace with your directory
+ftp.retrlines('LIST')
+
+def uploadFile():
+ filename = 'source.txt' #replace with your file in your home folder
+ ftp.storbinary('STOR '+filename, open(filename, 'rb'))
+ ftp.quit()
+
+def downloadFile():
+ filename = 'mantap.txt' #replace with your file in the directory ('directory_name')
+ localfile = open(filename, 'wb')
+ ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+ ftp.quit()
+ localfile.close()
+
+uploadFile()
+#downloadFile()
